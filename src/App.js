bf81fefc1a2257ce03f0059e8b4a2ec64b7faa2d
Paawan Singal <paawansingal09@gmail.com>
2021-10-16 15:59:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+  const pageSize = 5;
+  const apikey = "0364ec79379147979eadfdc8ef0f5b59";
 
-export default App;
+  
+  const [progress, setprogress] = useState(10)
+  
+  const setProgress = (progress) => {
+    setprogress(progress)
+  };
+  
+
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <LoadingBar
+            color="#f11946"
+            progress={progress}
+            height={3}
+            // onLoaderFinished={() => setProgress(0)}
+          />
+          <Switch>
+            <Route exact path="/">
+              {" "}
+              <News
+                apikey={apikey}
+                setProgress={setProgress}
+                key="general"
+                pageSize={pageSize}
+                country="in"
+                category="general"
+              />{" "}
+            </Route>
+            <Route exact path="/business">
+              {" "}
+              <News
+                apikey={apikey}
+                setProgress={setProgress}
+                key="Business"
+                pageSize={pageSize}
+                country="in"
+                category="Business"
+              />{" "}
+            </Route>
+            <Route exact path="/entertainment">
+              {" "}
+              <News
+                apikey={apikey}
+                setProgress={setProgress}
+                key="Entertainment"
+                pageSize={pageSize}
+                country="in"
+                category="Entertainment"
+              />{" "}
+            </Route>
+            <Route exact path="/health">
+              {" "}
+              <News
+                apikey={apikey}
+                setProgress={setProgress}
+                key="Health"
+                pageSize={pageSize}
+                country="in"
+                category="Health"
+              />{" "}
+            </Route>
+            <Route exact path="/science">
+              {" "}
+              <News
+                apikey={apikey}
+                setProgress={setProgress}
+                key="Science"
+                pageSize={pageSize}
+                country="in"
+                category="Science"
+              />{" "}
+            </Route>
+            <Route exact path="/sports">
+              {" "}
+              <News
+                apikey={apikey}
+                setProgress={setProgress}
+                key="Sports"
+                pageSize={pageSize}
+                country="in"
+                category="Sports"
+              />{" "}
+            </Route>
+            <Route exact path="/technology">
+              {" "}
+              <News
+                apikey={apikey}
+                setProgress={setProgress}
+                key="Technology"
+                pageSize={pageSize}
+                country="in"
+                category="Technology"
+              />{" "}
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
---
+++ src/components/News.js
@@ -0,0 +1,165 @@
+import React, { useEffect, useState } from "react";
+import Loader from "./Loader";
+import NewsItem from "./NewsItem";
+import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+export default function News(props) {
+  const [articles, setarticles] = useState([]);
+  const [loading, setloading] = useState(true);
+  const [page, setpage] = useState(1);
+  const [totalResults, settotalResults] = useState(0);
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  document.title = `NewsMonkey - ${capitalizeFirstLetter(props.category)}`;
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = {
+  //     articles: [],
+  //     loading: true,
+  //     page: 1,
+  //     totalresults: 0,
+  //   };
+  //   document.title = `NewsMonkey - ${this.capitalizeFirstLetter(
+  //     props.category
+  //   )}`;
+  // }
+
+  const updatenews = async () => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apikey}&page=${props.page}&PageSize=${props.pageSize}`;
+    setloading(true);
+    let data = await fetch(url);
+    props.setProgress(30);
+    let parsedData = await data.json();
+    props.setProgress(70);
+
+    // this.setState({
+    //   articles: parsedData.articles,
+    //   totalresults: parsedData.totalResults,
+    //   loading: false,
+    // });
+
+    setarticles(parsedData.articles);
+    settotalResults(parsedData.totalReasults);
+    setloading(false);
+    props.setProgress(100);
+  };
+
+  useEffect(() => {
+    updatenews();
+  }, []);
+
+  // const handlenextPage = async () => {
+  //   setpage(page + 1)
+
+  //   updatenews();
+  // };
+
+  // const handleprevPage = async () => {
+  //  setpage(page - 1);
+
+  //   updatenews();
+  // };
+
+  const fetchMoreData = async () => {
+    
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=0364ec79379147979eadfdc8ef0f5b59&page=${page + 1}&PageSize=${props.pageSize}`;
+    setpage(page + 1);
+    // this.setState({ loading: true });
+    setloading(true);
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    // this.setState({
+    //   articles: this.state.articles.concat(parsedData.articles),
+    //   totalresults: parsedData.totalResults,
+    //   loading: false,
+    // });
+
+    setarticles(articles.concat(parsedData.articles));
+    settotalResults(parsedData.totalReasults);
+    setloading(false);
+  };
+
+  return (
+    <>
+      <h1 className="text-center" style={{ margin: "21px 2px", marginTop : '90px' }}>
+        NewsMoney - Top {capitalizeFirstLetter(props.category)} Headlines
+      </h1>
+      {loading && <Loader />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Loader />}
+      >
+        {
+          <div className="container">
+            <div className="row">
+              {articles.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title}
+                      description={
+                        element.description
+                          ? element.description.slice(0, 80)
+                          : ""
+                      }
+                      imageUrl={element.urlToImage}
+                      newsUrl={element.url}
+                      publishedDate={element.publishedAt}
+                      author={element.author}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        }
+      </InfiniteScroll>
+
+      {/* 
+        there are the next and the previous buttons --- 
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleprevPage}
+          >
+            &larr; Previous
+          </button>
+          <button
+            disabled={
+              this.state.page >
+              Math.ceil(this.state.totalresults / props.pageSize)
+            }
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlenextPage}
+          >
+            Next &rarr;
+          </button>
+        </div> */}
+    </>
+  );
+}
+
+News.defaultProps = {
+  country: "in",
+  pageSize: 8,
+  category: "general",
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
